a47805ac40a6218e84cfdaa4645415bd7a3c5d35
sumit-6 <75926522+sumit-6@users.noreply.github.com>
2022-04-18 20:38:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,29 +35,6 @@ def index():
         return  render_template('play.html',data = data)
     else:
         return render_template('values.html')
-'''
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = ToDo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
 
-@app.route('/update/<int:id>', methods = ['GET', 'POST'])
-def update(id):
-    task = ToDo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue updating your task"
-    else:
-        return render_template('update.html', task = task)
-'''
 if __name__ == '__main__':
     app.run(debug = True)
